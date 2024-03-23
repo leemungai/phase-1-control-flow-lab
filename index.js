e0b398b2f79142c92457879d5a62b9267ea67045
leemungai <mrmungai@yahoo.com>
2024-03-23 12:16:32 +0100

    complete control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+
+  if (distance <= 400){
+    return "This one is on me!";
+  }
+  else if (distance > 400 && distance <2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+
+ } else if (distance > 2500) {
+   return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city, result) {
+  //if(city === 'NYC'){
+      //return 'Ok, sounds good.'
+      // else 'Pittsburgh';{
+      //   return 'No go'
+      // }
+      
+  //result = city === 'NYC' ?
+  result = city ==='NYC' ? "Ok, sounds good." : "No go."
+    return result
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip) {
+//  if (tip >= 'generous') {
+//   return 'Thank you so much.'
+//  }
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+      case 'not as generous' :
+        return 'Thank you.'
+        break;
+        default :
+        return 'Bye.'
+  }
+        
+
 }
